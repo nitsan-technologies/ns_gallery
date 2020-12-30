254a4afe41b175ccfcb2350dc71a5cf88e8a7abb
Milan Rathod <rmilan.nitsan@gmail.com>
2020-12-30 11:39:08 +0530

    :bug: [Bugfix]PHP Warning

--- Classes/Domain/Repository/NsAlbumRepository.php
+++ Classes/Domain/Repository/NsAlbumRepository.php
@@ -30,110 +30,112 @@ class NsAlbumRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
     public function setSettingsForGallery($settings, $constant)
     {
         $txtSettings = '';
-        foreach ($constant as $key => $value) {
-            if ($key == 'arrowIcon'  || $key == 'zoomIcon'  || $key == 'loadingIcon'  || $key == 'GlobalPagingPosition' || $key == 'layout' || $key == 'detailPageId' || $key == 'filterPosition' || $key == 'paginationType' || $key == 'addClass' || $key == 'PagingPosition') {
-            } else {
-                if ($key == 'videoControls') {
-                    $txtSettings .= 'controls:' . (isset($settings[$key]) && $settings[$key] !='' ? $settings[$key] : $constant[$key]) . ',';
+
+        if($constant) {
+            foreach ($constant as $key => $value) {
+                if ($key == 'arrowIcon'  || $key == 'zoomIcon'  || $key == 'loadingIcon'  || $key == 'GlobalPagingPosition' || $key == 'layout' || $key == 'detailPageId' || $key == 'filterPosition' || $key == 'paginationType' || $key == 'addClass' || $key == 'PagingPosition') {
                 } else {
-                    if ($key == 'cssEasing') {
-                        $keyData = isset($settings[$key]) && $settings[$key] !='' ? $settings[$key] : $constant[$key];
-                        switch ($keyData) {
-                            case '1':
-                              $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.250, 0.250, 0.750, 0.750)',";
-                            break;
-                            case '2':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.250, 0.100, 0.250, 1.000)',";
-                            break;
-                            case '3':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.420, 0.000, 1.000, 1.000)',";
-                            break;
-                            case '4':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.000, 0.000, 0.580, 1.000)',";
-                            break;
-                            case '5':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.420, 0.000, 0.580, 1.000)',";
-                            break;
-                            case '6':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.550, 0.085, 0.680, 0.530)',";
-                            break;
-                            case '7':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.550, 0.055, 0.675, 0.190)',";
-                            break;
-                            case '8':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.895, 0.030, 0.685, 0.220)',";
-                            break;
-                            case '9':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.755, 0.050, 0.855, 0.060)',";
-                            break;
-                            case '10':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.470, 0.000, 0.745, 0.715)',";
-                            break;
-                            case '11':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.950, 0.050, 0.795, 0.035)',";
-                            break;
-                            case '12':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.600, 0.040, 0.980, 0.335)',";
-                            break;
-                            case '13':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.600, -0.280, 0.735, 0.045)',";
-                            break;
-                            case '14':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.250, 0.460, 0.450, 0.940)',";
-                            break;
-                            case '15':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.215, 0.610, 0.355, 1.000)',";
-                            break;
-                            case '16':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.165, 0.840, 0.440, 1.000)',";
-                            break;
-                            case '17':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.230, 1.000, 0.320, 1.000)',";
-                            break;
-                            case '18':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.390, 0.575, 0.565, 1.000)',";
-                            break;
-                            case '19':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.190, 1.000, 0.220, 1.000)',";
-                            break;
-                            case '20':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.075, 0.820, 0.165, 1.000)',";
-                            break;
-                            case '21':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.175, 0.885, 0.320, 1.275)',";
-                            break;
-                            case '22':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.455, 0.030, 0.515, 0.955)',";
-                            break;
-                            case '23':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.645, 0.045, 0.355, 1.000)',";
-                            break;
-                            case '24':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.770, 0.000, 0.175, 1.000)',";
-                            break;
-                            case '25':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.860, 0.000, 0.070, 1.000)',";
-                            break;
-                            case '26':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.445, 0.050, 0.550, 0.950)',";
-                            break;
-                            case '27':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(1.000, 0.000, 0.000, 1.000)',";
-                            break;
-                            case '28':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.785, 0.135, 0.150, 0.860)',";
-                            break;
-                            case '29':
-                                $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.680, -0.550, 0.265, 1.550)',";
-                            break;
-                        }
+                    if ($key == 'videoControls') {
+                        $txtSettings .= 'controls:' . (isset($settings[$key]) && $settings[$key] !='' ? $settings[$key] : $constant[$key]) . ',';
                     } else {
-                        $txtSettings .= $key . ':' . (isset($settings[$key]) && $settings[$key] !='' ? $settings[$key] : $constant[$key]) . ',';
+                        if ($key == 'cssEasing') {
+                            $keyData = isset($settings[$key]) && $settings[$key] !='' ? $settings[$key] : $constant[$key];
+                            switch ($keyData) {
+                                case '1':
+                                  $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.250, 0.250, 0.750, 0.750)',";
+                                break;
+                                case '2':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.250, 0.100, 0.250, 1.000)',";
+                                break;
+                                case '3':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.420, 0.000, 1.000, 1.000)',";
+                                break;
+                                case '4':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.000, 0.000, 0.580, 1.000)',";
+                                break;
+                                case '5':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.420, 0.000, 0.580, 1.000)',";
+                                break;
+                                case '6':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.550, 0.085, 0.680, 0.530)',";
+                                break;
+                                case '7':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.550, 0.055, 0.675, 0.190)',";
+                                break;
+                                case '8':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.895, 0.030, 0.685, 0.220)',";
+                                break;
+                                case '9':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.755, 0.050, 0.855, 0.060)',";
+                                break;
+                                case '10':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.470, 0.000, 0.745, 0.715)',";
+                                break;
+                                case '11':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.950, 0.050, 0.795, 0.035)',";
+                                break;
+                                case '12':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.600, 0.040, 0.980, 0.335)',";
+                                break;
+                                case '13':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.600, -0.280, 0.735, 0.045)',";
+                                break;
+                                case '14':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.250, 0.460, 0.450, 0.940)',";
+                                break;
+                                case '15':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.215, 0.610, 0.355, 1.000)',";
+                                break;
+                                case '16':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.165, 0.840, 0.440, 1.000)',";
+                                break;
+                                case '17':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.230, 1.000, 0.320, 1.000)',";
+                                break;
+                                case '18':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.390, 0.575, 0.565, 1.000)',";
+                                break;
+                                case '19':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.190, 1.000, 0.220, 1.000)',";
+                                break;
+                                case '20':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.075, 0.820, 0.165, 1.000)',";
+                                break;
+                                case '21':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.175, 0.885, 0.320, 1.275)',";
+                                break;
+                                case '22':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.455, 0.030, 0.515, 0.955)',";
+                                break;
+                                case '23':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.645, 0.045, 0.355, 1.000)',";
+                                break;
+                                case '24':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.770, 0.000, 0.175, 1.000)',";
+                                break;
+                                case '25':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.860, 0.000, 0.070, 1.000)',";
+                                break;
+                                case '26':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.445, 0.050, 0.550, 0.950)',";
+                                break;
+                                case '27':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(1.000, 0.000, 0.000, 1.000)',";
+                                break;
+                                case '28':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.785, 0.135, 0.150, 0.860)',";
+                                break;
+                                case '29':
+                                    $txtSettings .= 'cssEasing:' . "'cubic-bezier(0.680, -0.550, 0.265, 1.550)',";
+                                break;
+                            }
+                        } else {
+                            $txtSettings .= $key . ':' . (isset($settings[$key]) && $settings[$key] !='' ? $settings[$key] : $constant[$key]) . ',';
+                        }
                     }
                 }
             }
-        }
-        $textData = "<script>
+            $textData = "<script>
                 (function($) {
                     $(window).load(function() {                            
                         $('#nsGallery-" . $getContentId . "').lightGallery({
@@ -143,6 +145,8 @@ class NsAlbumRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
                     });
                 })(jQuery);
             </script>';
+        }
+
         return $txtSettings;
     }
 
