6261cbdff399b4260091cd5a76f9517250c5fdf5
NITSAN Technologies <sanjay@nitsan.in>
2022-08-09 15:02:52 +0530

    [TASK] Change URL of t3terminal into t3planet

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF['ns_gallery'] = [
     'title' => '[NITSAN] All In One Gallery',
-    'description' => 'Do you want to integrate feature-rich gallery solution to your TYPO3 site? The extension includes wide-range of gallery album variations. Demo: https://demo.t3terminal.com/t3t-extensions/gallery You can download PRO version for more-features & free-support at https://t3terminal.com/ns-gallery-typo3-extension',
+    'description' => 'Do you want to integrate feature-rich gallery solution to your TYPO3 site? The extension includes wide-range of gallery album variations. Demo: https://demo.t3planet.com/t3t-extensions/gallery You can download PRO version for more-features & free-support at https://t3planet.com/ns-gallery-typo3-extension',
     'category' => 'plugin',
     'author' => 'Team NITSAN',
     'author_email' => 'sanjay@nitsan.in',
