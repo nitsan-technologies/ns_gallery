d2d1ffde647179a91d800c2f888b0370277f7989
NITSAN Technologies <sanjay@nitsan.in>
2022-08-10 18:42:58 +0530

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF['ns_gallery'] = [
     'title' => '[NITSAN] All In One Gallery',
-    'description' => 'Do you want to integrate feature-rich gallery solution to your TYPO3 site? The extension includes wide-range of gallery album variations. Demo: https://demo.t3planet.com/t3t-extensions/gallery You can download PRO version for more-features & free-support at https://t3planet.com/ns-gallery-typo3-extension',
+    'description' => 'Do you want to integrate feature-rich gallery solution to your TYPO3 site? The extension includes wide-range of gallery album variations. Demo: https://demo.t3terminal.com/t3t-extensions/gallery You can download PRO version for more-features & free-support at https://t3terminal.com/ns-gallery-typo3-extension',
     'category' => 'plugin',
     'author' => 'Team NITSAN',
     'author_email' => 'sanjay@nitsan.in',
@@ -12,7 +12,7 @@ $EM_CONF['ns_gallery'] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.1.0',
+    'version' => '2.1.1',
     'constraints' => [
         'depends' => [
             'typo3' => '8.0.0-11.5.99',
