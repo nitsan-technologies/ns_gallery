44378ecf01e3ee767b87553ff6bc8bd9b3beed33
Harsora Krupa <harsorakrupa.nitsan@gmail.com>
2024-04-22 17:41:12 +0530

    Remove Nitsan

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,4 +2,4 @@
 
 defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ns_gallery', 'Configuration/TypoScript', '[NITSAN] Gallery');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ns_gallery', 'Configuration/TypoScript', 'Gallery');
